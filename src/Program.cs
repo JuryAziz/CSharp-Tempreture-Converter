f5c5ae5611b081884b4b9254452711f5a69790bb
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-03-18 14:17:52 +0300

    Basic requirments - tempConvert function

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿static string TempConvert(double temp, string unit)
+{
+
+    switch (unit.ToUpper())
+    {
+        case "C":
+            return temp * 9/5 + 32 + " F";
+
+        case "F":
+            return  (temp - 32) * 5 / 9 + " C";
+
+        default:
+            return "Oops something went wrong";
+    }
+}
+
+Console.WriteLine(TempConvert(32, "F"));
+Console.WriteLine(TempConvert(100, "C"));
+
