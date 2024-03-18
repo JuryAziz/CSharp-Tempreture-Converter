cb04d2da3b44d56a3219a462a3ea3bca069f6a08
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-03-18 15:49:29 +0300

    advanced requirements - loop and exception handling

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
-﻿static string TempConvert(double temperature, string unit){
+﻿static string TempConvert(double temperature, string unit)
+{
 
-    switch (unit.ToUpper()){
+    switch (unit)
+    {
         case "C":
             return String.Format("{0:0.##}", temperature * 9 / 5 + 32) + " F";
 
@@ -8,15 +10,40 @@
             return String.Format("{0:0.##}", (temperature - 32) * 5 / 9) + " C";
 
         default:
-            return "Oops something went wrong";
+            throw new Exception("\t Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.");
     }
 }
 
-Console.Write("Enter a temperature and its unit (C or F): ");
-string[] inputs = Console.ReadLine().Split(" ");
+while (true)
+{
+    try
+    {
+        Console.Write("\n Enter a temperature and its unit (C or F), or type 'Quit' to exit: ");
+        string[] inputs = Console.ReadLine().Split(" ");
 
-double temperature = Convert.ToDouble(inputs[0]);
-string unit = Convert.ToString(inputs[1]);
 
-Console.WriteLine($"Converted : {temperature} {unit} = " + TempConvert(temperature, unit));
+        if (inputs[0].ToUpper() == "QUIT")
+        {
+            Console.WriteLine(" Program terminated. \n");
+            break;
+        }
 
+        if (inputs.Length < 2) throw new Exception("\t Invalid inout. Missing one or more arguments.");
+
+        if (!inputs[0].All(char.IsNumber)) throw new Exception("\t Invalid input. Please enter a numeric temperature.");
+
+
+        double temperature = Convert.ToDouble(inputs[0]);
+        string unit = inputs[1].ToUpper();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"\t Converted : {temperature} {unit} = " + TempConvert(temperature, unit));
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
